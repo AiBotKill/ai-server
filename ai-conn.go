b7be1af42b5366836dc4634d2c5ef76aa7bb3fd4
Antti Vesanen <antti.vesanen@iki.fi>
2015-02-15 03:22:05 +0200

    forgot to commit ai-conn.go

--- ai-conn.go
+++ ai-conn.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"time"
 )
 
-const NATS_TIMEOUT = time.Second * 3
+const NATS_TIMEOUT = time.Second * 30
 
 type Reader interface {
 	Read() (line string, err error)
@@ -28,12 +29,15 @@ type ReadWriteCloser interface {
 type AiConn struct {
 	Conn   ReadWriteCloser
 	TeamId string
-	BotIds []string
+	BotId  string
+	GameId string
+	State  string
 }
 
 func NewAiConn(conn ReadWriteCloser) *AiConn {
 	a := &AiConn{}
 	a.Conn = conn
+	a.State = "unregistered"
 	return a
 }
 
@@ -45,6 +49,14 @@ func (a *AiConn) Parser() {
 			if err != nil {
 				LogError("Error while reading from ai connection, closing: " + err.Error())
 				a.Conn.Close()
+				if a.State == "registered" || a.State == "joined" {
+					err = natsEncodedConn.Publish("unregisterAI", map[string]interface{}{
+						"botId": a.BotId,
+					})
+					if err != nil {
+						LogError("Can't unregister ai:", err.Error())
+					}
+				}
 				return
 			}
 			LogDebug("got string", line)
@@ -52,41 +64,134 @@ func (a *AiConn) Parser() {
 			// If teamId has not been set yet, expect the first message
 			// to be register message. Keep trying until registration
 			// is acknowledged by ai-console.
-			if a.TeamId == "" {
+			if a.State == "unregistered" {
 				var regMsg RegisterMessage
 				err := json.Unmarshal([]byte(line), &regMsg)
 				if err != nil {
-					e := "Registration message unmarshal error: " + err.Error()
-					LogWarn(e)
-					a.Conn.Write(NewJsonError(e))
+					a.LogErr(err)
 					continue
 				}
 
 				// Request registration through NATS
-				var regReply RegisterReply
-				err = natsEncodedConn.Request("registerAi", regMsg, &regReply, NATS_TIMEOUT)
+				var regReply Reply
+				err = natsEncodedConn.Request("registerAI", regMsg, &regReply, NATS_TIMEOUT)
 				if err != nil {
-					e := "Registration message publish error: " + err.Error()
-					LogError(e)
-					a.Conn.Write(NewJsonError(e))
+					a.LogErr(err)
 					continue
+				}
+
+				// DEBUG
+				if b, err := json.Marshal(&regReply); err == nil {
+					LogDebug("Got reply!", string(b))
 				}
 
 				// Check that reply status is ok.
 				if regReply.Status != "ok" {
-					e := "Registration not ok for team " + regMsg.TeamId + ": " + regReply.Error
-					LogWarn(e)
-					a.Conn.Write(line)
+					err := errors.New("Registration not ok for team " + regMsg.TeamId + ": " + regReply.Error)
+					a.LogErr(err)
 					continue
 				}
 
 				// All ok, Set TeamId
-				a.TeamId = regReply.Id
-				a.Conn.Write(line)
+				a.BotId = regReply.Id
+				b, _ := json.Marshal(regReply)
+				a.Conn.Write(string(b))
+				a.State = "registered"
+
+				natsEncodedConn.Subscribe("aiserver."+a.BotId, a.MsgHandler)
+				LogDebug("IT WORKS!")
 				continue
+			}
+
+			if a.State == "registered" {
+				var joinMsg JoinGame
+				err := json.Unmarshal([]byte(line), &joinMsg)
+				if err != nil {
+					a.LogErr(err)
+					continue
+				}
+
+				var reply Reply
+				err = natsEncodedConn.Request(joinMsg.GameId+".join", joinMsg, &reply, NATS_TIMEOUT)
+				if err != nil {
+					a.LogErr(err)
+					continue
+				}
+				if reply.Status != "ok" {
+					a.LogErr(errors.New(reply.Error))
+					continue
+				}
+
+				a.GameId = joinMsg.GameId
+				a.State = "joined"
+			}
+
+			if a.State == "joined" {
+				if GetMsgType([]byte(line)) == "action" {
+					var action ActionRequest
+					err := json.Unmarshal([]byte(line), action)
+					if err != nil {
+						a.LogErr(err)
+						continue
+					}
+
+					var reply Reply
+					if err := natsEncodedConn.Request(a.GameId+".action", action, &reply, NATS_TIMEOUT); err != nil {
+						a.LogErr(err)
+						continue
+					}
+
+					replBytes, err := json.Marshal(&reply)
+					if err != nil {
+						a.LogErr(err)
+						continue
+					}
+
+					a.Conn.Write(string(replBytes))
+				}
 			}
 		}
 	}()
+}
+
+func (c *AiConn) LogErr(err error) {
+	LogError(err.Error())
+	b, _ := json.Marshal(map[string]string{
+		"status": "error",
+		"error":  err.Error(),
+	})
+	c.Conn.Write(string(b))
+}
+
+func (c *AiConn) MsgHandler(subj string, reply string, msg []byte) {
+	switch c.State {
+	case "registered":
+		var m JoinRequest
+		err := json.Unmarshal(msg, &m)
+		if err != nil {
+			LogError("Error unmarshalling joinrequest", err.Error())
+		}
+		c.Conn.Write(string(msg))
+	case "joined":
+		switch GetMsgType(msg) {
+		case "gamestate":
+			c.Conn.Write(string(msg))
+		case "gameEnd":
+			c.Conn.Write(string(msg))
+			c.Conn.Close()
+		}
+	}
+}
+
+func GetMsgType(b []byte) string {
+	var m struct {
+		Type string `json:"type"`
+	}
+	err := json.Unmarshal(b, m)
+	if err != nil {
+		return ""
+	}
+	return m.Type
 }
 
 func NewJsonError(e string) string {
@@ -107,8 +212,34 @@ type RegisterMessage struct {
 	Version string `json:"version"`
 }
 
-type RegisterReply struct {
+type Reply struct {
 	Status string `json:"status"`
 	Id     string `json:"id"`
 	Error  string `json:"error,omitempty"`
+}
+
+type JoinRequest struct {
+	Type     string `json:"type"`
+	GameId   string `json:"gameId"`
+	GameMode string `json:"gameMode"`
+}
+
+type GameState struct {
+	Type string `json:"type"`
+}
+
+type ActionRequest struct {
+	BotId     string `json:"botid"`
+	Type      string `json:"type"`
+	Action    string `json:"action"`
+	Direction struct {
+		X float64 `json:"x"`
+		Y float64 `json:"y"`
+	} `json:"direction"`
+}
+
+type JoinGame struct {
+	Type   string `json:"joinGame"`
+	BotId  string `json:"botid"`
+	GameId string `jsno:"gameId"`
 }
